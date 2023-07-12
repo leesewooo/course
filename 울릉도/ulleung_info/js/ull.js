fac3fb869e62699456f7c1929790b085bd755100
leesewooo <139292865+leesewooo@users.noreply.github.com>
2023-07-12 09:28:14 +0900

    Add files via upload

---
+++ 울릉도/ulleung_info/js/ull.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#info a").hover(
+    function(){
+      $ (this).addClass("act");
+    },
+    function(){
+      $(this).removeClass("act")
+      
+    }
+  );
+});//끝부분
